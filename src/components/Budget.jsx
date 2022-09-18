270652896670451107e8ed9db8cf394b3874e952
Yanis Falaki <yanisfalaki@outlook.com>
2022-09-18 17:27:35 -0400

    Added ability to change budget

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,11 +1,53 @@
 import React, { useContext } from "react";
+import { useState } from "react";
 import { AppContext } from "../context/AppContext";
 
 export const Budget = () => {
-  const { budget } = useContext(AppContext);
+  const { budget, dispatch } = useContext(AppContext);
+  const [editing, setEditing] = useState(false);
+  const [display, setDisplay] = useState(<span>Budget: ${budget}</span>);
+  const [newBudget, setNewBudget] = useState();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    if (!editing) {
+      dispatch({
+        type: "CHANGE_BUDGET",
+        payload: newBudget,
+      });
+    }
+
+    setNewBudget();
+  };
+
+  const budgetDisplay = () => {
+    setEditing(!editing);
+    if (!editing) {
+      setDisplay(
+        <input
+          type={"number"}
+          className="form-control me-5"
+          value={newBudget}
+          onChange={(event) => setNewBudget(event.target.value)}
+        ></input>
+      );
+    } else {
+      setDisplay(<span>Budget: ${newBudget}</span>);
+    }
+  };
+
   return (
-    <div className="alert alert-secondary">
-      <span>Budget: ${budget}</span>
+    <div className="alert alert-secondary d-flex align-items-center">
+      {display}
+      <form className="ms-auto" onSubmit={onSubmit}>
+        <button
+          className="btn btn-primary ms-auto"
+          onClick={() => budgetDisplay()}
+        >
+          {!editing ? "Edit" : "Save"}
+        </button>
+      </form>
     </div>
   );
 };
